b2da81ed3abdf26d6b2a187970f58c2efbfe6a8c
matys1 <ragout.burdens0n@icloud.com>
2024-06-26 10:36:12 +0200

    feat: added 3rd client (trip status); need to make it reactive now

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { wasmConnector, coordinator, MosaicClient, Selection, clauseInterval, Param } from "@uwdata/mosaic-core";
+import { wasmConnector, coordinator, MosaicClient, Selection, clauseInterval, clausePoints, Param } from "@uwdata/mosaic-core";
 import { Query, count } from "@uwdata/mosaic-sql";
 import testData from "./data.js";
 
@@ -70,18 +70,23 @@ class CustomClient extends MosaicClient {
 const selection = Selection.crossfilter();
 const client1 = new CustomClient("testData", "HourOfDay", selection, (data) => console.log(data) /* (data) => comp1.data(data) */ );
 const client2 = new CustomClient("testData", "DayOfWeek", selection, (data) => console.log(data) /* (data) => comp2.data(data) */ );
+const client3 = new CustomClient("testData", "TripStatus", selection, (data) => console.log(data) /* (data) => comp3.data(data) */ );
 await coord.connect(client1);
 await coord.connect(client2);
+await coord.connect(client3);
 
-const c1 = Param.value(0);
-const c2 = Param.value(24);
-const c3 = Param.value(0);
-const c4 = Param.value(7);
+const c1_x0 = Param.value(0);
+const c1_x1 = Param.value(24);
+const c2_x0 = Param.value(0);
+const c2_x1 = Param.value(7);
+// const c3 = Param.array(["Not Started", "In Transit", "Draft", "Arrived", "Disabled", "Started"]);
 
-selection.update(clauseInterval("HourOfDay", [c1, c2], { source: client1 }));
-selection.update(clauseInterval("DayOfWeek", [c3, c4], { source: client2 }));
+selection.update(clauseInterval("HourOfDay", [c1_x0, c1_x1], { source: client1 }));
+selection.update(clauseInterval("DayOfWeek", [c2_x0, c2_x1], { source: client2 }));
+selection.update(clausePoints(["TripStatus"], [["In Transit"]], { source: client3 }));
 
-c1.update(2);
-c2.update(3);
-c3.update(0);
-c4.update(1);
+c1_x0.update(2);
+c1_x1.update(3);
+c2_x0.update(0);
+c2_x1.update(1);
+// c3.update(["In Transit"]);
